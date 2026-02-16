a9d6f16ea058b06301ae123c3498b650a91307ba
rohitsunilsharma2000 <rohitsunilsharma2000@gmail.com>
2026-02-16 20:09:30 +0530

    refactor: remove deprecated demo files and components from advanced topics

--- advanced-topics/demos/snippets/validation-utils.ts
+++
@@ -1,6 +0,0 @@
-import { AbstractControl, ValidationErrors } from '@angular/forms';
-export function endAfterStart(ctrl: AbstractControl): ValidationErrors | null {
-  const start = ctrl.get('start')?.value;
-  const end = ctrl.get('end')?.value;
-  return start && end && end < start ? { endBeforeStart: true } : null;
-}
